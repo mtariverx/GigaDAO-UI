5de7404bec26ede67e0fb6187889bd17a05cdd88
mtariverx <warrensinclair744@gmail.com>
2022-05-02 23:06:32 +0800

    reorganized UI

--- src/pages/AdminDashBoard.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout'
-const AdminDashBoard:React.FC=props=>{
-    return (
-    <Layout>
-        AdminDashBoard
-    </Layout>)
-}
-
-export default AdminDashBoard
